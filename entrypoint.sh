ce6e07717f1a8c8df7b9bc599f241cd3287ab1ce
artnicanov <artnicanov@gmail.com>
2023-05-27 15:22:54 +0300

    lesson 34

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 python manage.py migrate --check
 status=$?
-if [[ status != 0 ]]; then
+if [[ $status != 0 ]]; then
   python manage.py migrate
 fi
 python manage.py collectstatic -c --no-input
