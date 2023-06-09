639e28861bf799afd06c2eb6ebe7deb570c66734
artnicanov <artnicanov@gmail.com>
2023-06-10 02:53:19 +0300

    tests done

---
+++ tests/boards/test_board_view.py
@@ -0,0 +1,102 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from goals.models import BoardParticipant
+
+
+@pytest.mark.django_db()
+class TestBoardRetrieveView:
+    @pytest.fixture(autouse=True)
+    def setup(self, board_participant):
+        self.url = self.get_url(board_participant.board_id)
+
+    @staticmethod
+    def get_url(board_pk: int) -> str:
+        return reverse('goals:board', kwargs={'pk': board_pk})
+
+    def test_auth_required(self, client):
+        """Неавторизованный пользователь не может просматривать доски."""
+        response = client.get(self.url)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_failed_to_retrieve_deleted_board(self, auth_client, board):
+        """Пользователь не может просматривать удалённые доски."""
+        board.is_deleted = True
+        board.save()
+
+        response = auth_client.get(self.url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_failed_to_retrieve_foreign_board(self, client, user_factory):
+        """Пользователь не может просматривать доски, где он не является участником."""
+        another_user = user_factory.create()
+        client.force_login(another_user)
+
+        response = client.get(self.url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND  # должно быть 403
+
+    @pytest.mark.parametrize(
+        'role',
+        [
+            BoardParticipant.Role.owner,
+            BoardParticipant.Role.writer,
+            BoardParticipant.Role.reader,
+        ],
+        ids=['owner', 'writer', 'reader'],
+    )
+    def test_of_getting_board_by_participant_with_any_role(
+        self, client, board, role, user_factory, board_participant_factory
+    ):
+        """Доску может просмотреть любой пользователь, являющийся её участником"""
+        another_user = user_factory.create()
+        board_participant_factory.create(user=another_user, board=board, role=role)
+        client.force_login(another_user)
+
+        response = client.get(self.url)
+
+        assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db()
+class TestBoardDestroyView:
+    @pytest.fixture(autouse=True)
+    def setup(self, board_participant):
+        self.url = self.get_url(board_participant.board_id)
+
+    @staticmethod
+    def get_url(board_pk: int) -> str:
+        return reverse('goals:board', kwargs={'pk': board_pk})
+
+    def test_auth_required(self, client):
+        """Неавторизованный пользователь не может удалять доски."""
+        response = client.delete(self.url)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    @pytest.mark.parametrize(
+        'role',
+        [
+            BoardParticipant.Role.writer,
+            BoardParticipant.Role.reader,
+        ],
+        ids=['writer', 'reader'],
+    )
+    def test_not_owner_failed_to_delete_board(self, client, user_factory, board, board_participant_factory, role):
+        """Пользователь не являющийся владельцем доски, не может её удалить."""
+        another_user = user_factory.create()
+        board_participant_factory.create(user=another_user, board=board, role=role)
+        client.force_login(another_user)
+
+        response = client.delete(self.url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_owner_have_to_delete_board(self, auth_client, board):
+        """Пользователь являющийся владельцем доски, может её удалить."""
+        response = auth_client.delete(self.url)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        board.refresh_from_db()
+        assert board.is_deleted is True
---
+++ tests/categories/test_category_create_view.py
@@ -0,0 +1,25 @@
+from typing import Callable
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+
+@pytest.fixture()
+def category_create_data(faker) -> Callable:
+    def _wrapper(**kwargs) -> dict:
+        data = {'title': faker.sentence(2)}
+        data |= kwargs
+        return data
+
+    return _wrapper
+
+
+@pytest.mark.django_db()
+class TestCategoryCreateView:
+    url = reverse('goals:category-create')
+
+    def test_auth_required(self, client, category_create_data):
+        """Неавторизованный пользователь при создании категории получит ошибку авторизации."""
+        response = client.post(self.url, data=category_create_data())
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
